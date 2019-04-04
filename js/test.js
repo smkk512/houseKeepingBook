cc46ab7324c2c33220332c6227b856421d567a7b
KSY <smkk512@naver.com>
2019-04-04 23:33:15 +0900

    quicksort

---
+++ js/test.js
@@ -0,0 +1,24 @@
+
+let list = new Array(10);
+
+const RandomList = _list =>{
+    let randList = [];
+    
+    for(i=0;i<_list.length;i++){
+        _list[i] = i;
+    }
+    
+    
+    
+}
+
+const QuickSort = () => {
+    
+}
+
+const init = () => {
+    RandomList(list);
+    console.log(list);
+}
+
+init();
